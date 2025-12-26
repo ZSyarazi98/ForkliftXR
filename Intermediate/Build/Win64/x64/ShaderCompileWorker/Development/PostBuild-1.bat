891813354462629827e081ad91df1126c4b47b99
ZSyarazi98 <zurisyarazi@gmail.com>
2025-12-26 11:09:24 +0800

    Git LFS Initialized

---
+++ Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Copying D:\UnrealEngineMeta\Engine\Binaries\Win64\ShaderCompileWorker.exe to D:\UnrealEngineMeta\Engine\Binaries\Win64\XGEControlWorker.exe
+copy /Y /B "D:\UnrealEngineMeta\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "D:\UnrealEngineMeta\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
